aa1768481c0c0da2cdc8e6e450afb63fbecbbf42
meyer30 <meyer30@illinois.edu>
2017-06-09 22:27:15 -0500

    now displaying rest of nation details

--- main.js
+++ main.js
@@ -33,7 +33,13 @@ function SearchBtn_Click() {
 }
 
 function OnSuccess(response) {
-    //$('#div-results').empty();
+    var resultsDiv = document.getElementById('div-results');
+    resultsDiv.innerHTML = "";
+    
+//    
+// 
+//At the bottom of the page show the total number of countries and list all regions and subregions contained in the results with the number of times it appeared. 
+    
     if(Array.isArray(response)){     
         for(var idx=0; idx<response.length; idx++){
             AddNationToView(response[idx]);
@@ -50,6 +56,21 @@ function OnFail(result) {
 }
 
 function AddNationToView(nation){ 
+    
+//    For each country displayed include: the full name, alpha code 2, alpha code 3, flag image (scaled to fit display), 
+//    region, subregion, population, and a list of its languages. 
     var resultsDiv = document.getElementById('div-results');
     resultsDiv.innerHTML += "<div>"+nation.name+"</div>";
+    resultsDiv.innerHTML += "<div>"+nation.alpha2Code+"</div>";
+    resultsDiv.innerHTML += "<div>"+nation.alpha3Code+"</div>";
+    resultsDiv.innerHTML += "<img src='" +nation.flag+"'/>";
+    resultsDiv.innerHTML += "<div>"+nation.region+"</div>";
+    resultsDiv.innerHTML += "<div>"+nation.subregion+"</div>";
+    resultsDiv.innerHTML += "<div>"+nation.population+"</div>";
+        for(var idx=0; idx<nation.languages.length; idx++){
+            resultsDiv.innerHTML += "<div>"+nation.languages[idx]+"</div>";
+        }       
+    //flag
+//:
+//"https://restcountries.eu/data/umi.svg"
 }
